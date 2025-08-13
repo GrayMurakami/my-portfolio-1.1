5d7e4a39e8aac887ffcba9e9a33ccde2e32e328a
Gray <digrayman8@gmail.com>
2025-08-13 17:44:15 +0900

    some fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    }
+  },
   css: [
     '~/assets/css/main.css',
   ],
